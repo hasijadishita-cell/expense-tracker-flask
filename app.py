de7803c4363db9b0c673f0494c16bdd83a2d3c9d
Dishita Hasija <dishitahasija@Dishitas-MacBook-Air.local>
2026-01-11 00:49:38 +1100

    Add category-based monthly total filtering

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ def init_db():
 CREATE TABLE IF NOT EXISTS expenses(
                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                    description TEXT NOT NULL,
-                   amount REAL NOT NULL)
+                   amount REAL NOT NULL,
+                   category TEXT NOT NULL,
+                   expense_date DATE NOT NULL)
                    """)
     con.commit()
     con.close()
@@ -30,19 +32,29 @@ def index():
     if request.method=="POST":
         description=request.form.get("description")
         amount=float(request.form["amount"])
+        category=request.form.get("category")
+        expense_date=request.form["expense_date"]
 
-        cursor.execute("INSERT INTO EXPENSES (description, amount) VALUES (?, ?)", (description, amount))
+        cursor.execute("INSERT INTO EXPENSES (description, amount, category, expense_date) VALUES (?, ?, ?, ?)", (description, amount, category, expense_date))
         con.commit()
         return redirect("/")
     
-    cursor.execute("SELECT * FROM expenses")
+    filter_category=request.args.get("filter_category")
+    if filter_category and filter_category!="all":
+        cursor.execute("SELECT * FROM expenses WHERE CATEGORY=? ORDER BY expense_date DESC", (filter_category,))
+    else:
+        cursor.execute("SELECT * FROM expenses ORDER BY expense_date DESC")
     expenses=cursor.fetchall()
 
-    cursor.execute("SELECT SUM(amount) FROM expenses")
-    total=cursor.fetchone()[0]
-    total=total if total else 0
+    if filter_category and filter_category!="all":
+        cursor.execute("SELECT SUM(amount) FROM expenses WHERE CATEGORY=? AND strftime('%Y-%m', expense_date)= strftime('%Y-%m', 'now')", (filter_category,))
+    else:
+        cursor.execute("SELECT SUM(amount) FROM expenses WHERE strftime('%Y-%m', expense_date)= strftime('%Y-%m', 'now')")
+
+    month_total=cursor.fetchone()[0]
+    month_total=month_total if month_total else 0
     con.close()
-    return render_template("index.html", expenses=expenses, total=total)
+    return render_template("index.html", expenses=expenses, month_total=month_total, filter_category=filter_category)
 
 @app.route("/delete/<int:expense_id>", methods=["POST"])
 
